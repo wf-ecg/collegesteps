e5f89bf9b06f4f89f4f19512f9c5f9f42789e238
dvdrtrgn <dvdrtrgn@gmail.com>
2015-06-25 16:52:46 -0500

    restore specificity by reordering css

--- tasks/default.js
+++ tasks/default.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
         'jshint:postcat',
         //'uglify',
         'sync:clean',
-        //'connect:full',
+        'connect:full',
         'watch',
     ]);
 
