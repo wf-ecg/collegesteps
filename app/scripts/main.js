37fac299e72afe4b6b29e993604cd323d3df6b9f
dvdrtrgn <dvdrtrgn@gmail.com>
2015-06-30 17:15:05 -0500

    move navmenu init into main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -197,6 +197,17 @@
         if ($.scrollUp) $.scrollUp({
             animation: 'fade',
         });
+
+        $('nav:first').accessibleMegaMenu({
+            uuidPrefix: 'accessible-megamenu',
+            menuClass: 'nav-menu',
+            topNavItemClass: 'nav-item',
+            panelClass: 'sub-nav',
+            panelGroupClass: 'sub-nav-group',
+            hoverClass: 'hover',
+            focusClass: 'focus',
+            openClass: 'open',
+        });
     });
 
 
