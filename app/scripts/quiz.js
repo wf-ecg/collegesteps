493a325a475318e29636df7c552be0afd9541d8d
dvdrtrgn <dvdrtrgn@gmail.com>
2015-07-01 10:42:14 -0500

    refactor quiz

--- app/scripts/quiz.js
+++ app/scripts/quiz.js
@@ -6,56 +6,64 @@
      * Easy jQuery-Based Quiz ~ Copyright (c) 2012 Meredith Dodge, http://meredithdodge.com
      * Released under MIT license
      */
-    var imgpath = "img/";
-    var jsonpath = "json/";
-    var jsonfile = "";
+    var imgpath = 'images/';
+    var jsonpath = 'data/';
+    var jsonfile = '';
 
-    $(W.document).ready(function () {
-        $("input[type='radio']").attr("disabled", false);
-        $("input[type='radio']").attr("checked", false);
-        $(".submit").click(function (a) {
-            a.preventDefault();
+    $(function () {
+        $('input[type="radio"]').attr('disabled', false);
+        $('input[type="radio"]').attr('checked', false);
+        $('.submit').click(function (evt) {
+            evt.preventDefault();
             checkQuiz();
         });
-        jsonfile = $("#quiz").attr("rel") + ".json";
+        jsonfile = $('#quiz').attr('rel') + '.json';
     });
 
-    function getData(a) {
-        $.getJSON(jsonpath + jsonfile, function (b) {
-            a(b);
+    function getData(cb) {
+        $.getJSON(jsonpath + jsonfile, function (data) {
+            cb(data);
         }).error(function () {
-            alert("error");
+            alert('error');
         });
     }
 
     function checkQuiz() {
-        $(".submit").remove();
-        getData(function (c) {
-            var b = c.key;
-            var a = {};
-            $(".question").each(function () {
-                var f = $(this).attr("id");
-                var e = $("#" + f + " input:checked").closest("li").attr("class");
-                a[f] = e;
-                $("#" + f + " ." + b[f]).append("<img src='" + imgpath + "circle.png' class='png_bg' alt='correct answer'>");
-                if (b[f] == e) {
-                    $(this).addClass("correct");
+        $('.submit').remove();
+
+        getData(function (json) {
+            var keys = json.key;
+            var obj = {};
+            var str;
+
+            $('.question').each(function () {
+                var me = $(this);
+                var id = me.attr('id');
+                var cla = $('#' + id + ' input:checked').closest('li').attr('class');
+
+                obj[id] = cla;
+                $('#' + id + ' .' + keys[id]).append('<img src="' + imgpath + 'circle.png" class="png_bg" alt="correct answer">');
+
+                if (keys[id] == cla) {
+                    me.addClass('correct');
                 } else {
-                    $(this).addClass("wrong");
+                    me.addClass('wrong');
                 }
             });
-            var d = "You got " + $(".correct").length + " out of " + $(".question").length + " correct. ";
-            if ($(".correct").length <= 4) {
-                d += "<div>Don't worry, now's a great time to start gaining more knowledge about credit – read on to start understanding credit today!</div>";
+
+            str = 'You got ' + $('.correct').length + ' out of ' + $('.question').length + ' correct. ';
+
+            if ($('.correct').length <= 4) {
+                str += '<div>Don’t worry, now’s a great time to start gaining more knowledge about credit—read on to start understanding credit today!</div>';
             } else {
-                if ($(".correct").length >= 8) {
-                    d += "<div>Congratulations, you're smart about credit! Read on to learn even more.</div>";
+                if ($('.correct').length >= 8) {
+                    str += '<div>Congratulations, you’re smart about credit! Read on to learn even more.</div>';
                 } else {
-                    d += "<div>You've got some of the basics down, but keep reading to brush up on your credit knowledge</div>";
+                    str += '<div>You’ve got some of the basics down, but keep reading to brush up on your credit knowledge</div>';
                 }
             }
-            $(".feedback").html(d);
-            $(".feedback").show();
+
+            $('.feedback').html(str).show();
         });
     }
 
