0f96f089d131340b1d9f321163c2fc459169e092
dvdrtrgn <dvdrtrgn@gmail.com>
2016-05-25 11:04:47 -0500

    compile wip: hotfix vids

--- app/scripts/_main.js
+++ app/scripts/_main.js
@@ -2,323 +2,323 @@
 /*global define, window, _V_ */
 
 define(['jquery', 'modal', 'xhr',
-    'bootstrap', 'megamenu', 'sidebar', 'waypoints',
-    'skrollr', 'scrollit', 'scrollto', 'scrollup', 'quiz'
+  'bootstrap', 'megamenu', 'sidebar', 'waypoints',
+  'skrollr', 'scrollit', 'scrollto', 'scrollup', 'quiz'
 ], function ($, Modal, XHR) {
-    'use strict';
-    var W = window, C = W.console, navbar, sidbar;
+  'use strict';
+  var W = window, C = W.console, navbar, sidbar;
 
-    navbar = '#navbar';
-    sidbar = '#menu-4';
+  navbar = '#navbar';
+  sidbar = '#menu-4';
 
-    $.fn.hasAttr = function (attr) {
-        return this.attr(attr) !== undefined;
-    };
+  $.fn.hasAttr = function (attr) {
+    return this.attr(attr) !== undefined;
+  };
 
-    function navsize1() {
-        var winh = $(W).height();
-        var navbarheight = navbar.outerHeight();
-        var home = $('#home');
+  function navsize1() {
+    var winh = $(W).height();
+    var navbarheight = navbar.outerHeight();
+    var home = $('#home');
 
-        if (home.hasClass('home-fullscreen') && navbar.hasClass('nav-home-bottom')) {
-            home.css('height', winh - navbarheight);
-        } else if (home.hasClass('home-fullscreen')) {
-            $('#home').css('height', winh);
-        }
-
-        if (winh < 600) {
-            $('.logo-home').hide();
-        } else {
-            $('.logo-home').show();
-        }
+    if (home.hasClass('home-fullscreen') && navbar.hasClass('nav-home-bottom')) {
+      home.css('height', winh - navbarheight);
+    } else if (home.hasClass('home-fullscreen')) {
+      $('#home').css('height', winh);
     }
 
-    function navsize2() {
-        $(W).scroll(function () {
-            if (navbar.hasClass('nav-fixed') && navbar.hasClass('nav-home-top') && $(W).scrollTop() >= 1) {
-                navbar.addClass('nav-fixed-fixed');
-            } else {
-                navbar.removeClass('nav-fixed-fixed');
-            }
-        });
-        if (navbar.hasClass('nav-home-top')) {
-            $('.main-nav ul li:first-child a').addClass('active');
-            $('.main-nav ul li ul li a').removeClass('active');
-            $('.logo-home').css('display', 'none');
-        }
-        if (navbar.hasClass('nav-home-bottom')) {
-            navbar.addClass('submenu-up');
-        }
-        $(W).scroll(function () {
-            if (navbar.hasClass('nav-home-bottom') && $(W).scrollTop() >= 130) {
-                navbar.removeClass('submenu-up');
-            } else {
-                if (navbar.hasClass('nav-home-bottom')) {
-                    navbar.addClass('submenu-up');
-                }
-            }
-        });
+    if (winh < 600) {
+      $('.logo-home').hide();
+    } else {
+      $('.logo-home').show();
     }
+  }
 
-    function navTrig() {
-        var sidebarTrigger = $('#st-trigger-effects');
-
-        if (navbar.hasClass('nav-fixed')) {
-            navbar.scrollToFixed();
-        }
-        if (sidebarTrigger.hasClass('nav-fixed')) {
-            sidebarTrigger.scrollToFixed();
-        }
-    }
-
-    function navClick() {
-        $('.main-nav ul li a').click(function (l) {
-            var ref = $(this).attr('href');
-
-            if (ref === '#' || ref === '') {
-                l.preventDefault();
-            }
-        });
-        $.scrollIt();
-    }
-
-    function trifEff() {
-        var cla = 'st-menu-open';
-        var ele = $('#st-container');
-        var scrtopvar;
-
-        $('#st-trigger-effects a').click(function () {
-            scrtopvar = $(W).scrollTop();
-        });
-
-        $(W).scroll(function () {
-            var scrtop = $(W).scrollTop();
-            var scrolloffset = 10;
-
-            if (ele.hasClass(cla)) {
-                if (scrtopvar >= (scrtop + scrolloffset) || scrtopvar <= (scrtop - scrolloffset)) {
-                    ele.removeClass(cla);
-                }
-            }
-        });
-    }
-
-    function faders() {
-        var animTime = 700;
-        var fadeDelay;
-        var limit = '90%';
-
-        function prep(m) {
-            if ($(m).hasAttr('fade-delay')) {
-                fadeDelay = $(m).attr('fade-delay');
-            } else {
-                fadeDelay = 0;
-            }
-        }
-        $('.fadeLeft').each(function () {
-            $(this).waypoint(function () {
-                prep(this);
-                $(this).delay(fadeDelay).animate({
-                    opacity: 1,
-                    left: '0px'
-                }, animTime);
-            }, {
-                offset: limit
-            });
-        });
-        $('.fadeRight').each(function () {
-            $(this).waypoint(function () {
-                prep(this);
-                $(this).delay(fadeDelay).animate({
-                    opacity: 1,
-                    right: '0px'
-                }, animTime);
-            }, {
-                offset: limit
-            });
-        });
-        $('.fadeTop').each(function () {
-            $(this).waypoint(function () {
-                prep(this);
-                $(this).delay(fadeDelay).animate({
-                    opacity: 1,
-                    top: '0px'
-                }, animTime);
-            }, {
-                offset: limit
-            });
-        });
-        $('.fadeBottom').each(function () {
-            $(this).waypoint(function () {
-                prep(this);
-                $(this).delay(fadeDelay).animate({
-                    opacity: 1,
-                    bottom: '0px'
-                }, animTime);
-            }, {
-                offset: limit
-            });
-        });
-        $('.fadeIn').each(function () {
-            $(this).waypoint(function () {
-                prep(this);
-                $(this).delay(fadeDelay).animate({
-                    opacity: 1
-                }, animTime);
-            }, {
-                offset: limit
-            });
-        });
-    }
-
-    function quizzer() {
-        $('fieldset').on('click', 'input', function (evt) {
-            $(evt.delegateTarget).find('div').hide('fast');
-            $(evt.target).parent().next().show('fast');
-        });
-    }
-
-    function drSkroll() {
-        if (!Boolean('ontouchstart' in W || 'onmsgesturechange' in W)) {
-            if (W.skrollr) {
-                W.skrollr.init();
-            }
-        }
-    }
-
-    function drEtc() {
-        $('#page-loader').delay(500).fadeOut('slow');
-
-        $(W).load(function () {
-            $('.loader').delay(300).fadeOut();
-        }).resize(function () {
-            navsize1();
-            navsize2();
-        });
-
-        if ($.scrollUp) {
-            $.scrollUp({
-                animation: 'fade',
-            });
-        }
-        $('nav:first').accessibleMegaMenu({
-            uuidPrefix: 'accessible-megamenu',
-            menuClass: 'nav-menu',
-            topNavItemClass: 'nav-item',
-            panelClass: 'sub-nav',
-            panelGroupClass: 'sub-nav-group',
-            hoverClass: 'hover',
-            focusClass: 'focus',
-            openClass: 'open',
-        });
-    }
-
-    function drVid() {
-        if (W._V_) {
-            _V_('video_1').ready(function () {
-                var myPlayer = this;
-                var aspectRatio = 9 / 16;
-                var resizeVideoJS = function () {
-                    var width = W.document.getElementById(myPlayer.id).parentElement.offsetWidth;
-                    myPlayer.width(width).height(width * aspectRatio);
-                };
-
-                resizeVideoJS();
-                W.onresize = resizeVideoJS;
-            });
-        }
-    }
-
-    function drSetnav() {
-        var inum, item, navi;
-
-        navi = $('body').data('navi');
-        if (!navi) {
-            return;
-        }
-        inum = (navi - 1);
-        item = navbar.find('.nav-item').eq(inum);
-
-        navbar.find('.nav-item').not(item) // get other page links
-            .find('.sub-nav-group a').attr('data-scroll-nav', -1) // disable scroll
-            .click(function () { // passthru click
-                W.location = this.href;
-            });
-
-        item.addClass('active') // indicate current
-            .find('a').first().attr('href', '#') // disable link
-            .attr('data-scroll-nav', 0); // scroll to top
-
-        sidbar.find('a').eq(inum) // get link to current page
-            .attr('data-scroll-nav', 0) // disable
-            .attr('href', '#') // disable
-            .addClass('active');
-    }
-
-    function footnotes() {
-        // find data-footnotes
-        var trigs = $('[data-footnote]');
-        var ACT = 'keypress.footnote click.footnote';
-
-        // turn into triggers
-        trigs.each(function () {
-            var trig = $(this);
-            var targ = trig.data('footnote');
-
-            // target the value as classname
-            targ = $('.footnote-' + targ).attr('tabindex', 0);
-            trig.attr('tabindex', 0);
-
-            // on target.ACT focus is returned to trigger
-            trig.on(ACT, function () {
-                targ.focus()
-                    .off(ACT)
-                    .on(ACT, function () {
-                        trig.focus();
-                    });
-            });
-        });
-    }
-
-    function bindModal() {
-        var dialog = $('.modal .dialog'); // thing to show
-        var triggers = $('#stickyBar .shares a'); // intercept these
-
-        Modal.bind(triggers, dialog, function (data) {
-            dialog.find('.utilitybtn') // find the go button
-                .attr('href', data.source[0].href); // transfer url
-        });
-    }
-
-    function watchInputDevice() {
-        var body = $('body').on('keydown', function () {
-            body.removeClass('mouse').addClass('keyboard');
-        }).on('mousemove', function () {
-            body.removeClass('keyboard').addClass('mouse');
-        });
-    }
-
-    function init() {
-        navbar = $(navbar);
-        sidbar = $(sidbar);
-
-        navsize1();
-        navTrig();
-        navClick();
-        trifEff();
-        faders();
-        navsize2();
-        quizzer();
-        drSkroll();
-        drVid();
-        drSetnav();
-        footnotes();
-        bindModal();
-        watchInputDevice();
-        drEtc();
-    }
-
-    _.defer(function () {
-        XHR.doit(init);
+  function navsize2() {
+    $(W).scroll(function () {
+      if (navbar.hasClass('nav-fixed') && navbar.hasClass('nav-home-top') && $(W).scrollTop() >= 1) {
+        navbar.addClass('nav-fixed-fixed');
+      } else {
+        navbar.removeClass('nav-fixed-fixed');
+      }
     });
+    if (navbar.hasClass('nav-home-top')) {
+      $('.main-nav ul li:first-child a').addClass('active');
+      $('.main-nav ul li ul li a').removeClass('active');
+      $('.logo-home').css('display', 'none');
+    }
+    if (navbar.hasClass('nav-home-bottom')) {
+      navbar.addClass('submenu-up');
+    }
+    $(W).scroll(function () {
+      if (navbar.hasClass('nav-home-bottom') && $(W).scrollTop() >= 130) {
+        navbar.removeClass('submenu-up');
+      } else {
+        if (navbar.hasClass('nav-home-bottom')) {
+          navbar.addClass('submenu-up');
+        }
+      }
+    });
+  }
+
+  function navTrig() {
+    var sidebarTrigger = $('#st-trigger-effects');
+
+    if (navbar.hasClass('nav-fixed')) {
+      navbar.scrollToFixed();
+    }
+    if (sidebarTrigger.hasClass('nav-fixed')) {
+      sidebarTrigger.scrollToFixed();
+    }
+  }
+
+  function navClick() {
+    $('.main-nav ul li a').click(function (l) {
+      var ref = $(this).attr('href');
+
+      if (ref === '#' || ref === '') {
+        l.preventDefault();
+      }
+    });
+    $.scrollIt();
+  }
+
+  function trifEff() {
+    var cla = 'st-menu-open';
+    var ele = $('#st-container');
+    var scrtopvar;
+
+    $('#st-trigger-effects a').click(function () {
+      scrtopvar = $(W).scrollTop();
+    });
+
+    $(W).scroll(function () {
+      var scrtop = $(W).scrollTop();
+      var scrolloffset = 10;
+
+      if (ele.hasClass(cla)) {
+        if (scrtopvar >= (scrtop + scrolloffset) || scrtopvar <= (scrtop - scrolloffset)) {
+          ele.removeClass(cla);
+        }
+      }
+    });
+  }
+
+  function faders() {
+    var animTime = 700;
+    var fadeDelay;
+    var limit = '90%';
+
+    function prep(m) {
+      if ($(m).hasAttr('fade-delay')) {
+        fadeDelay = $(m).attr('fade-delay');
+      } else {
+        fadeDelay = 0;
+      }
+    }
+    $('.fadeLeft').each(function () {
+      $(this).waypoint(function () {
+        prep(this);
+        $(this).delay(fadeDelay).animate({
+          opacity: 1,
+          left: '0px'
+        }, animTime);
+      }, {
+        offset: limit
+      });
+    });
+    $('.fadeRight').each(function () {
+      $(this).waypoint(function () {
+        prep(this);
+        $(this).delay(fadeDelay).animate({
+          opacity: 1,
+          right: '0px'
+        }, animTime);
+      }, {
+        offset: limit
+      });
+    });
+    $('.fadeTop').each(function () {
+      $(this).waypoint(function () {
+        prep(this);
+        $(this).delay(fadeDelay).animate({
+          opacity: 1,
+          top: '0px'
+        }, animTime);
+      }, {
+        offset: limit
+      });
+    });
+    $('.fadeBottom').each(function () {
+      $(this).waypoint(function () {
+        prep(this);
+        $(this).delay(fadeDelay).animate({
+          opacity: 1,
+          bottom: '0px'
+        }, animTime);
+      }, {
+        offset: limit
+      });
+    });
+    $('.fadeIn').each(function () {
+      $(this).waypoint(function () {
+        prep(this);
+        $(this).delay(fadeDelay).animate({
+          opacity: 1
+        }, animTime);
+      }, {
+        offset: limit
+      });
+    });
+  }
+
+  function quizzer() {
+    $('fieldset').on('click', 'input', function (evt) {
+      $(evt.delegateTarget).find('div').hide('fast');
+      $(evt.target).parent().next().show('fast');
+    });
+  }
+
+  function drSkroll() {
+    if (!Boolean('ontouchstart' in W || 'onmsgesturechange' in W)) {
+      if (W.skrollr) {
+        W.skrollr.init();
+      }
+    }
+  }
+
+  function drEtc() {
+    $('#page-loader').delay(500).fadeOut('slow');
+
+    $(W).load(function () {
+      $('.loader').delay(300).fadeOut();
+    }).resize(function () {
+      navsize1();
+      navsize2();
+    });
+
+    if ($.scrollUp) {
+      $.scrollUp({
+        animation: 'fade',
+      });
+    }
+    $('nav:first').accessibleMegaMenu({
+      uuidPrefix: 'accessible-megamenu',
+      menuClass: 'nav-menu',
+      topNavItemClass: 'nav-item',
+      panelClass: 'sub-nav',
+      panelGroupClass: 'sub-nav-group',
+      hoverClass: 'hover',
+      focusClass: 'focus',
+      openClass: 'open',
+    });
+  }
+
+  function drVid() {
+    if (W._V_) {
+      _V_('video_1').ready(function () {
+        var myPlayer = this;
+        var aspectRatio = 9 / 16;
+        var resizeVideoJS = function () {
+          var width = W.document.getElementById(myPlayer.id).parentElement.offsetWidth;
+          myPlayer.width(width).height(width * aspectRatio);
+        };
+
+        resizeVideoJS();
+        W.onresize = resizeVideoJS;
+      });
+    }
+  }
+
+  function drSetnav() {
+    var inum, item, navi;
+
+    navi = $('body').data('navi');
+    if (!navi) {
+      return;
+    }
+    inum = (navi - 1);
+    item = navbar.find('.nav-item').eq(inum);
+
+    navbar.find('.nav-item').not(item) // get other page links
+        .find('.sub-nav-group a').attr('data-scroll-nav', -1) // disable scroll
+        .click(function () { // passthru click
+          W.location = this.href;
+        });
+
+    item.addClass('active') // indicate current
+        .find('a').first().attr('href', '#') // disable link
+        .attr('data-scroll-nav', 0); // scroll to top
+
+    sidbar.find('a').eq(inum) // get link to current page
+        .attr('data-scroll-nav', 0) // disable
+        .attr('href', '#') // disable
+        .addClass('active');
+  }
+
+  function footnotes() {
+    // find data-footnotes
+    var trigs = $('[data-footnote]');
+    var ACT = 'keypress.footnote click.footnote';
+
+    // turn into triggers
+    trigs.each(function () {
+      var trig = $(this);
+      var targ = trig.data('footnote');
+
+      // target the value as classname
+      targ = $('.footnote-' + targ).attr('tabindex', 0);
+      trig.attr('tabindex', 0);
+
+      // on target.ACT focus is returned to trigger
+      trig.on(ACT, function () {
+        targ.focus()
+            .off(ACT)
+            .on(ACT, function () {
+              trig.focus();
+            });
+      });
+    });
+  }
+
+  function bindModal() {
+    var dialog = $('.modal .dialog'); // thing to show
+    var triggers = $('#stickyBar .shares a'); // intercept these
+
+    Modal.bind(triggers, dialog, function (data) {
+      dialog.find('.utilitybtn') // find the go button
+          .attr('href', data.source[0].href); // transfer url
+    });
+  }
+
+  function watchInputDevice() {
+    var body = $('body').on('keydown', function () {
+      body.removeClass('mouse').addClass('keyboard');
+    }).on('mousemove', function () {
+      body.removeClass('keyboard').addClass('mouse');
+    });
+  }
+
+  function init() {
+    navbar = $(navbar);
+    sidbar = $(sidbar);
+
+    navsize1();
+    navTrig();
+    navClick();
+    trifEff();
+    faders();
+    navsize2();
+    quizzer();
+    drSkroll();
+    drVid();
+    drSetnav();
+    footnotes();
+    bindModal();
+    watchInputDevice();
+    drEtc();
+  }
+
+  _.defer(function () {
+    XHR.doit(init);
+  });
 });
--- app/scripts/quiz.js
+++ app/scripts/quiz.js
@@ -2,82 +2,82 @@
 /*global jQuery */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 define(['jquery'], function ($) {
-    /*!
-     * Easy jQuery-Based Quiz ~ Copyright (c) 2012 Meredith Dodge, http://meredithdodge.com
-     * Released under MIT license
-     */
-    var imgpath = 'images/';
-    var jsonpath = 'data/';
-    var jsonfile = '';
-    var main = '#quiz';
+  /*!
+   * Easy jQuery-Based Quiz ~ Copyright (c) 2012 Meredith Dodge, http://meredithdodge.com
+   * Released under MIT license
+   */
+  var imgpath = 'images/';
+  var jsonpath = 'data/';
+  var jsonfile = '';
+  var main = '#quiz';
 
-    $(function () {
-        main = $(main);
-        if (!main.length) {
-            return;
-        }
-        $('label').each(function (i, e) { // ensure the inputs have explicit label
-            var lab = $(e);
-            var inp = lab.prev();
-            var nom = 'Inp' + i;
+  $(function () {
+    main = $(main);
+    if (!main.length) {
+      return;
+    }
+    $('label').each(function (i, e) { // ensure the inputs have explicit label
+      var lab = $(e);
+      var inp = lab.prev();
+      var nom = 'Inp' + i;
 
-            lab.attr('for', nom);
-            inp.attr('id', nom);
-        });
-        $('input[type="radio"]').attr('disabled', false);
-        $('input[type="radio"]').prop('checked', false); // from jq migrate
-        $('.submit').click(function (evt) {
-            evt.preventDefault();
-            checkQuiz();
-        });
-        jsonfile = main.attr('rel') + '.json';
+      lab.attr('for', nom);
+      inp.attr('id', nom);
     });
+    $('input[type="radio"]').attr('disabled', false);
+    $('input[type="radio"]').prop('checked', false); // from jq migrate
+    $('.submit').click(function (evt) {
+      evt.preventDefault();
+      checkQuiz();
+    });
+    jsonfile = main.attr('rel') + '.json';
+  });
 
-    function getData(cb) {
-        $.getJSON(jsonpath + jsonfile, function (data) {
-            cb(data);
-        }).error(function () {
-            alert('error');
-        });
-    }
+  function getData(cb) {
+    $.getJSON(jsonpath + jsonfile, function (data) {
+      cb(data);
+    }).error(function () {
+      alert('error');
+    });
+  }
 
-    function checkQuiz() {
-        $('.submit').remove();
+  function checkQuiz() {
+    $('.submit').remove();
 
-        getData(function (json) {
-            var keys = json.key;
-            var obj = {};
-            var str;
+    getData(function (json) {
+      var keys = json.key;
+      var obj = {};
+      var str;
 
-            $('.question').each(function () {
-                var me = $(this);
-                var id = me.attr('id');
-                var cla = $('#' + id + ' input:checked').closest('li').attr('class');
+      $('.question').each(function () {
+        var me = $(this);
+        var id = me.attr('id');
+        var cla = $('#' + id + ' input:checked').closest('li').attr('class');
 
-                obj[id] = cla;
-                $('#' + id + ' .' + keys[id]).append('<img src="' + imgpath + 'icon/circle.png" class="png_bg" alt="correct answer">');
+        obj[id] = cla;
+        $('#' + id + ' .' + keys[id]).append('<img src="' + imgpath + 'icon/circle.png" class="png_bg" alt="correct answer">');
 
-                if (keys[id] === cla) {
-                    me.addClass('correct');
-                } else {
-                    me.addClass('wrong');
-                }
-            });
+        if (keys[id] === cla) {
+          me.addClass('correct');
+        } else {
+          me.addClass('wrong');
+        }
+      });
 
-            str = 'You got ' + $('.correct').length + ' out of ' + $('.question').length + ' correct. ';
+      str = 'You got ' + $('.correct').length + ' out of ' + $('.question').length + ' correct. ';
 
-            if ($('.correct').length <= 4) {
-                str += '<div>Don’t worry, now’s a great time to start gaining more knowledge about credit—read on to start understanding credit today!</div>';
-            } else {
-                if ($('.correct').length >= 8) {
-                    str += '<div>Congratulations, you’re smart about credit! Read on to learn even more.</div>';
-                } else {
-                    str += '<div>You’ve got some of the basics down, but keep reading to brush up on your credit knowledge</div>';
-                }
-            }
+      if ($('.correct').length <= 4) {
+        str += '<div>Don’t worry, now’s a great time to start gaining more knowledge about credit—read on to start understanding credit today!</div>';
+      } else {
+        if ($('.correct').length >= 8) {
+          str += '<div>Congratulations, you’re smart about credit! Read on to learn even more.</div>';
+        } else {
+          str += '<div>You’ve got some of the basics down, but keep reading to brush up on your credit knowledge</div>';
+        }
+      }
 
-            $('.feedback').html(str).show();
-        });
-    }
+      $('.feedback').html(str).show();
+    });
+  }
 
 });
