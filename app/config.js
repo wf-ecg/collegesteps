0d385da446bc084346df406ecb834e3582e2156e
dvdrtrgn <dvdrtrgn@gmail.com>
2016-05-25 10:38:07 -0500

    tidy all formatting

--- app/config.js
+++ app/config.js
@@ -9,74 +9,74 @@ W.SHIET = {
 W.debug = Number(new Date('2015/10/01') > new Date());
 
 require.config({
-    baseUrl: 'scripts',
-    paths: {
-        lr: 'http://localhost:7200/livereload.js?snipver=1',
-        lib: 'libs',
-        ven: '../vendor',
-        jquery: '/lib/jquery/1.11.3/jquery',
-        bootstrap: '/lib/bootstrap/3.2.0/bootstrap',
-        lodash: '/lib/underscore/js-1.4.4/lodash.underscore',
-        modern: '/lib/modernizr/2.6.2/modernizr.min',
-        //
-        beacon: 'libs/ecg-beacon',
-        console: 'libs/console',
-        modal: 'libs/modal',
-        sidebar: 'libs/sidebarEffects',
-        stats: 'libs/ecg-stats',
-        //
-        megamenu: '../vendor/jquery-accessibleMegaMenu', //migrate: '../vendor/jquery-migrate-git',
-        skrollr: '../vendor/skrollr',
-        scrollit: '../vendor/scrollIt.min',
-        scrollto: '../vendor/jquery-scrolltofixed-min',
-        scrollup: '../vendor/jquery.scrollUp.min',
-        waypoints: '../vendor/waypoints.min',
+  baseUrl: 'scripts',
+  paths: {
+    lr: 'http://localhost:7200/livereload.js?snipver=1',
+    lib: 'libs',
+    ven: '../vendor',
+    jquery: '/lib/jquery/1.11.3/jquery',
+    bootstrap: '/lib/bootstrap/3.2.0/bootstrap',
+    lodash: '/lib/underscore/js-1.4.4/lodash.underscore',
+    modern: '/lib/modernizr/2.6.2/modernizr.min',
+    //
+    beacon: 'libs/ecg-beacon',
+    console: 'libs/console',
+    modal: 'libs/modal',
+    sidebar: 'libs/sidebarEffects',
+    stats: 'libs/ecg-stats',
+    //
+    megamenu: '../vendor/jquery-accessibleMegaMenu', //migrate: '../vendor/jquery-migrate-git',
+    skrollr: '../vendor/skrollr',
+    scrollit: '../vendor/scrollIt.min',
+    scrollto: '../vendor/jquery-scrolltofixed-min',
+    scrollup: '../vendor/jquery.scrollUp.min',
+    waypoints: '../vendor/waypoints.min',
+  },
+  shim: {
+    skrollr: {
+      deps: ['jquery'],
+      exports: 'skrollr',
     },
-    shim: {
-        skrollr: {
-            deps: ['jquery'],
-            exports: 'skrollr',
-        },
-        megamenu: ['jquery'],
-        scrollit: ['jquery'],
-        sidebar: ['jquery'],
-        scrollto: ['jquery'],
-        scrollup: ['jquery'],
-        waypoints: ['jquery'],
-        xhr: ['jquery'],
-    }
+    megamenu: ['jquery'],
+    scrollit: ['jquery'],
+    sidebar: ['jquery'],
+    scrollto: ['jquery'],
+    scrollup: ['jquery'],
+    waypoints: ['jquery'],
+    xhr: ['jquery'],
+  }
 });
 
 require(['modern'], function () {
-    try {
-        if (W.SHIET.trident) { // debug IE less
-            W.debug--;
-        }
-        if (W.location.hostname === 'localhost') { // debug local more
-            if (W.debug > 0) {
-                $('html').addClass('debug');
-            }
-            W.debug++;
-        }
-        if (W.debug > 0) { // any debug should attempt livereload
-            require(['lr']);
-            C.warn('LiveReloading');
-        }
-    } catch (err) {
-        C.error('config', err);
+  try {
+    if (W.SHIET.trident) { // debug IE less
+      W.debug--;
     }
+    if (W.location.hostname === 'localhost') { // debug local more
+      if (W.debug > 0) {
+        $('html').addClass('debug');
+      }
+      W.debug++;
+    }
+    if (W.debug > 0) { // any debug should attempt livereload
+      require(['lr']);
+      C.warn('LiveReloading');
+    }
+  } catch (err) {
+    C.error('config', err);
+  }
 
-    /// CUSTOM
+  /// CUSTOM
 
-    require(['lodash', 'jquery', '_main'], function (_) {
-        _.delay(function () {
-            if (W.debug < 2) {
-                require(['stats'], function (stats) {
-                    stats.init('COLLEGESTEPS');
-                });
-            }
-        }, 1e3);
-    });
+  require(['lodash', 'jquery', '_main'], function (_) {
+    _.delay(function () {
+      if (W.debug < 2) {
+        require(['stats'], function (stats) {
+          stats.init('COLLEGESTEPS');
+        });
+      }
+    }, 1e3);
+  });
 });
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
--- app/scripts/xhr.js
+++ app/scripts/xhr.js
@@ -2,30 +2,30 @@
 /*global define */
 
 define(['jquery'], function XHR($) {
-    var Cache = $('<div>');
+  var Cache = $('<div>');
 
-    function usurp(sel) {
-        var fill, self;
+  function usurp(sel) {
+    var fill, self;
 
-        self = $(sel);
-        fill = Cache.find(sel);
+    self = $(sel);
+    fill = Cache.find(sel);
 
-        self.append(fill.children());
-    }
+    self.append(fill.children());
+  }
 
-    function doit(done) {
-        Cache.load('_parts.html', function () {
-            usurp('#navbar');
-            usurp('#stickyBar');
-            usurp('#menu-4');
-            usurp('.externals');
-            usurp('.copyrights');
-            usurp('.modal');
-            done();
-        });
-    }
+  function doit(done) {
+    Cache.load('_parts.html', function () {
+      usurp('#navbar');
+      usurp('#stickyBar');
+      usurp('#menu-4');
+      usurp('.externals');
+      usurp('.copyrights');
+      usurp('.modal');
+      done();
+    });
+  }
 
-    return {
-        doit: doit,
-    };
+  return {
+    doit: doit,
+  };
 });
